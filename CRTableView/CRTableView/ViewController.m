70da91d5a4b04c33dbf9ffffcf086f7c8e1d88b5
Sean Chain <seanchain2.0@gmail.com>
2015-05-11 11:29:42 +0800

    update

--- CRTableView/CRTableView/ViewController.m
+++ CRTableView/CRTableView/ViewController.m
@@ -18,18 +18,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    CGRect statusBarFrame = [[UIApplication sharedApplication] statusBarFrame];
+    CGFloat barh = statusBarFrame.size.height;
+    ASFTableView *mytableview = [[ASFTableView alloc] initWithFrame:CGRectMake(0, barh, self.view.frame.size.width, self.view.frame.size.height)];
+    mytableview.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:mytableview];
     NSArray *cols = @[@"Person ID",@"Person Name",@"Phone Number",@"Sex"];
     NSArray *weights = @[@(0.15f),@(0.4f),@(0.25f),@(0.2f)];
     NSDictionary *options = @{kASF_OPTION_CELL_TEXT_FONT_SIZE : @(14),
                               kASF_OPTION_CELL_TEXT_FONT_BOLD : @(true),
                               kASF_OPTION_CELL_BORDER_COLOR : [UIColor lightGrayColor],
-                              kASF_OPTION_CELL_BORDER_SIZE : @(2.0),
+                              kASF_OPTION_CELL_BORDER_SIZE : @(0.0),
                               kASF_OPTION_BACKGROUND : [UIColor colorWithRed:239/255.0 green:244/255.0 blue:254/255.0 alpha:1.0]};
     
-    [_tableview setDelegate:self];
-    [_tableview setBounces:NO];
-    [_tableview setSelectionColor:[UIColor colorWithRed:242/255.0 green:242/255.0 blue:242/255.0 alpha:1.0f]];
-    [_tableview setTitles:cols
+    [mytableview setDelegate:self];
+    [mytableview setBounces:NO];
+    [mytableview setSelectionColor:[UIColor colorWithRed:242/255.0 green:242/255.0 blue:242/255.0 alpha:1.0f]];
+    [mytableview setTitles:cols
                   WithWeights:weights
                   WithOptions:options
                     WitHeight:32 Floating:YES];
@@ -49,12 +54,12 @@
                                 kASF_ROW_OPTIONS :
                                     @{kASF_OPTION_BACKGROUND : [UIColor whiteColor],
                                       kASF_OPTION_CELL_PADDING : @(5),
-                                      kASF_OPTION_CELL_BORDER_COLOR : [UIColor lightGrayColor]},
+                                      kASF_OPTION_CELL_BORDER_COLOR : [UIColor whiteColor]},
                                 
                                 @"some_other_data" : @(123)}];
     }
     
-    [_tableview setRows:_rowsArray];
+    [mytableview setRows:_rowsArray];
 }
 
 - (id)initWithCoder:(NSCoder *)aDecoder
